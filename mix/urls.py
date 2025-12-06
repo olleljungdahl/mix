225fd7b89ada4bc763cdf20eee752cb5e44d75db
olleljungdahl <olle@ljungdahl.com>
2025-12-06 17:09:37 +0100

    App, database, static file, images work

--- mix/urls.py
+++ mix/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django. conf.urls. static import static
+from django. conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='home'),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('about/', views.about_view, name='about'),
     path('posts/', include('posts.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
