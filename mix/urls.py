d37254c7e5fe863bffaa4b19b4da9c87d648d250
olleljungdahl <olle@ljungdahl.com>
2025-12-08 08:29:58 +0100

    login working after lesson 10

--- mix/urls.py
+++ mix/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django. conf.urls. static import static
-from django. conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='home'),
     path('', views.home_view),  # <--- Tomma citattecken betyder "startsidan"
     path('about/', views.about_view, name='about'),
-    path('posts/', include('posts.urls')),
-    path('users/', include('users.urls')),
+    path('posts/', include(('posts.urls', 'posts'), namespace='posts')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
